10d8ad1dbe0dbe6d8e7895e0e1b29981c3479384
tushar <swamitusharjee2018@gmail.com>
2023-12-22 18:06:29 +0530

    setting up first routes

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express=require('express');
 const port=8080;
+const router=express.Router();
 
 const app=express();
+
+
+app.use('/',require('./routes'));
 
 app.listen(port,(err)=>{
     if(err){
