474b8ca51c250286892191f89df928d9ea50200b
tushar <swamitusharjee2018@gmail.com>
2023-12-22 23:59:47 +0530

    if user sign in then don't acces page sign in or up page

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -5,11 +5,17 @@ module.exports.profile = function (req, res) {
   return res.render("profile", { title: "Codieal profile" });
 };
 module.exports.signup = function (req, res) {
+  if(req.isAuthenticated()){
+    return res.redirect('/user/profile');
+  }
   return res.render("user_sign_up", {
     title: "Codieal | Sign Up",
   });
 };
 module.exports.signin = function (req, res) {
+  if(req.isAuthenticated()){
+    return res.redirect('/user/profile');
+  }
   return res.render("user_sign_in", {
     title: "Codieal | Sign in",
   });
@@ -29,5 +35,5 @@ module.exports.create = async function (req, res) {
 };
 module.exports.createSession = function (req, res) {
   console.log(`done`);
-  return res.redirect("/");
+  return res.redirect("/user/profile");
 };
