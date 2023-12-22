eda493472e66aca58070648d947fcb4b4e3371b2
tushar <swamitusharjee2018@gmail.com>
2023-12-22 18:58:21 +0530

    sign up page complete data store in database

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,3 +1,6 @@
+const User=require('../models/user');
+
+
 module.exports.signup=function(req,res){
     return res.render('user_sign_up',{
         title:"Codieal | Sign Up"
@@ -8,8 +11,18 @@ module.exports.signin=function(req,res){
         title:"Codieal | Sign in"
     })
 }
-module.exports.create=function(req,res){
+module.exports.create=async function(req,res){
+   if(req.body.password!=req.body.confirmpassword){
+    return res.redirect('back');
+   }
+   const user=await User.findOne({email:req.body.email});
    
+   if(!user){
+    User.create(req.body);
+    return res.redirect('/user/sign-in');
+   }else{
+    return res.redirect('back');
+   }
 }
 module.exports.createSession=function(req,res){
     
--- routes/user.js
+++ routes/user.js
@@ -4,5 +4,6 @@ const router=express.Router();
 const userController=require('../controllers/user_controller');
 router.get('/sign-in',userController.signin);
 router.get('/sign-up',userController.signup);
+router.post('/create',userController.create);
 
 module.exports=router;
