4097328dc8a80357c1106af8c6e97d45da7b9290
tushar <swamitusharjee2018@gmail.com>
2023-12-22 18:17:06 +0530

    layout and partial setup

--- index.js
+++ index.js
@@ -2,10 +2,21 @@ const express=require('express');
 const port=8080;
 const router=express.Router();
 
+// ejs layout with express 
+const expressLayout=require('express-ejs-layouts');
+
 const app=express();
 
+
+// middle ware 
+app.use(expressLayout);
+
+
+// set view engine 
 app.set('view engine','ejs');
 app.set('views','views');
+
+// router calling 
 app.use('/',require('./routes'));
 
 app.listen(port,(err)=>{
