0a29a926c8543e16cc93466fc5df5beb83517bd5
tushar <swamitusharjee2018@gmail.com>
2023-12-23 17:59:46 +0530

    add sass to our project

--- index.js
+++ index.js
@@ -1,27 +1,29 @@
 const express = require("express");
 const port = 8080;
+const path=require('path');
 const router = express.Router();
 const mongoose = require("./config/mongoose");
 const cookieParser = require("cookie-parser");
 const session = require("express-session");
 const passport = require("passport");
 const passportLocal = require("./config/passport-local-stratergy");
+const sassMiddleWare = require("node-sass-middleware");
 
-const mongoStore=require('connect-mongo');
+const mongoStore = require("connect-mongo");
 // ejs layout with express
 const expressLayout = require("express-ejs-layouts");
 
 const app = express();
-
-
 
 // set view engine
 app.set("view engine", "ejs");
 app.set("views", "views");
 // middle ware
 app.use(expressLayout);
-app.use(express.urlencoded({extended:false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.set('layout extractStyles',true);
+app.set('layout extractScripts',true);
 
 // passport session
 app.use(
@@ -33,17 +35,29 @@ app.use(
     cookie: {
       maxAge: 1000 * 60 * 100,
     },
-    store:new mongoStore({
-      mongoUrl:'mongodb://127.0.0.1:27017/codieal',
-      autoRemove:'disabled'
-
-    })
+    store: new mongoStore({
+      mongoUrl: "mongodb://127.0.0.1:27017/codieal",
+      autoRemove: "disabled",
+    }),
   })
 );
 
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(passport.setAuthenticatedUser);
+
+// sass middleware use
+app.use(
+  sassMiddleWare({
+    src: "./assets/scss",
+    dest: "./assets/css",
+    debug: true,
+    outputStyle: "extended",
+    prefix: "/css",
+  })
+);
+app.use(express.static(path.resolve(__dirname+'/assets')));
+
 // router calling
 app.use("/", require("./routes"));
 
