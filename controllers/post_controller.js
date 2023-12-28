6824001d898d111d3802bbd01c7249eabe99b504
tushar <swamitusharjee2018@gmail.com>
2023-12-29 00:13:01 +0530

    deleting post from mongo

--- controllers/post_controller.js
+++ controllers/post_controller.js
@@ -1,4 +1,5 @@
 const Post=require('../models/post');
+const Comment=require('../models/comments');
 
 module.exports.create=function(req,res){
     Post.create({
@@ -6,4 +7,14 @@ module.exports.create=function(req,res){
         user:req.user._id
     });
     return res.redirect('back');
+}
+module.exports.destroy=async function(req,res){
+  const post=  await Post.findById(req.params.id).exec();
+  if(post.user==req.user.id){
+    await post.deleteOne();
+
+  }
+  await Comment.deleteMany({post:req.params.id});
+  return res.redirect('back');
+
 }
